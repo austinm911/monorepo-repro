f3be6803652c248ebac0a96fa3818e7681947f39
Austin Mudd <austinm911@gmail.com>
2025-09-19 10:18:50 -0700

    misc

--- apps/web/src/zshy-test.ts
+++ apps/web/src/zshy-test.ts
@@ -1,5 +1,7 @@
+import { db } from "server/db";
 import { publicProcedure } from "server/lib/orpc.ts";
 
 // import { publicProcedure } from "server/lib/orpc"; // error
 
 const _stub = publicProcedure;
+const _db = db;
